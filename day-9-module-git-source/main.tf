fe2d62fc1609ee6cd32b5ec07bc6e533ac87efd7
Saikumar <sakinm.saikumar95@gmail.com>
2025-12-07 17:58:40 +0530

    new

--- day-9-module-git-source/main.tf
+++ day-9-module-git-source/main.tf
@@ -1,6 +1,6 @@
 module "prod" {
-    source="value"
-  ami_id =  "ami-0c5204531f799e0c6"
+  source = "git::https://github.com/Sakinamsaikumar/terraform.git//day-9-modules"
+   ami_id =  "ami-0c5204531f799e0c6"
   instance_type = "t2.micro"
  # key = "test"
   availability_zone = "us-west-2a"
