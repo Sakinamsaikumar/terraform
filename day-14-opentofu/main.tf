ebb077717031c02a6215e537f5e030776ad51cdb
Saikumar <sakinm.saikumar95@gmail.com>
2025-12-17 07:01:48 +0530

    opentofu

---
+++ day-14-opentofu/main.tf
@@ -0,0 +1,14 @@
+resource "aws_vpc" "name" {
+  cidr_block = "10.0.0.0/16"
+    tags = {
+        Name = "tofuvpc"
+    }
+}
+
+
+
+resource "aws_subnet" "name" {
+    cidr_block = "10.0.0.0/24"
+    vpc_id     = aws_vpc.name.id
+  
+}
